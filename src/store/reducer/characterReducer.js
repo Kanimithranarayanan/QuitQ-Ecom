f5fcbbdfdb6f55e97e893b5035519e150e02690e
kanim <mithrakani102@gmail.com>
2026-06-19 15:23:41 +0530

    Update characterReducer.js

--- src/store/reducer/characterReducer.js
+++ src/store/reducer/characterReducer.js
@@ -1,19 +1,20 @@
 const initialState = {
-    characters: []
+    characters: [],
+    totalPages: 0
 };
 
-export const characterReducer = (
-    state = initialState,
-    action
-) => {
+export const characterReducer = (state = initialState, action) => {
 
-    if(action.type === "GET_ALL"){
+    switch (action.type) {
 
-        return {
-            ...state,
-            characters: action.payload
-        };
+        case "GET_ALL":
+            return {
+                ...state,
+                characters: action.payload.characters,
+                totalPages: action.payload.totalPages
+            };
+
+        default:
+            return state;
     }
-
-    return state;
 };
